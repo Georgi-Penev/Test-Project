2a745a0c309d283256ec38fcd8919ef458519e7f
Georgi-Penev <george.p.penev@gmail.com>
2025-01-09 18:59:22 +0200

    Testing

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,14 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+const baseURL = 'http://127.0.0.1:5500/site.html'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.describe('UI', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(baseURL);
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  test('correct title', async ({ page }) => {
+    await expect(page).toHaveTitle('Test');
+  });
 });
